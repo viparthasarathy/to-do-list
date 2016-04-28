0124becd0ae45b1af2b0561b158cf8fe204fb49f
viparthasarathy <viparthasarathy@gmail.com>
2016-04-27 20:45:41 -0700

    edit environment.rb and create database.yml

--- Gemfile
+++ Gemfile
@@ -22,3 +22,8 @@ group :test do
   gem 'rack-test'
   gem 'database_cleaner'
 end
+
+group :production do
+  gem 'pg'
+  gem 'activerecord-postgresql-adapter'
+end
--- config/environment.rb
+++ config/environment.rb
@@ -1,11 +1,16 @@
-ENV['SINATRA_ENV'] ||= "development"
+configure :development do
+  set :database, "sqlite3:///database.db"
+end
 
-require 'bundler/setup'
-Bundler.require(:default, ENV['SINATRA_ENV'])
+configure :production do
+  db = URI.parse(ENV['postgres://zmemoijynaclwq:kzAA8S0rOzrsi_BxbwZPpE3ZHn@ec2-54-163-245-162.compute-1.amazonaws.com:5432/dm636c9fkgilk'] || 'postgres://localhost/mydb')
 
-ActiveRecord::Base.establish_connection(
-  adapter: "sqlite3",
-  database: "db/#{ENV["SINATRA_ENV"]}.sqlite"
-)
-
-require_all 'app'
+  ActiveRecord::Base.establish_connection(
+    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+    :host     => db.host,
+    :username => db.user,
+    :password => db.password,
+    :database => db.path[1..-1],
+    :encoding => 'utf8'
+  )
+end
